cf077f519cc488b16f1843669e0ac106218a7758
icxd <icoding666@gmail.com>
2024-04-18 16:53:23 +0200

    shuisheg

--- cbe.c
+++ cbe.c
@@ -1,10 +1,13 @@
 #include "cbe.h"
 #include <stdio.h>
+#include <string.h>
 
 void cbe_init(struct cbe_context *ctx) {
   for (usz i = 0; i < CBE_REG_COUNT; i++)
     ctx->registers[i] = (struct cbe_register){i, cbe_register_name(i), false};
   slice_init(&ctx->stack_variables);
+  slice_init(&ctx->global_variables);
+  slice_init(&ctx->symbol_table);
 }
 
 usz cbe_next_unused_register(struct cbe_context *ctx) {
@@ -32,6 +35,36 @@ usz cbe_allocate_stack_variable(struct cbe_context *ctx) {
   usz index = ctx->stack_variables.size;
   slice_push(&ctx->stack_variables, (struct cbe_stack_variable){.slot = index});
   return index;
+}
+
+usz cbe_new_global_variable(struct cbe_context *ctx,
+                            struct cbe_global_variable variable) {
+  usz index = ctx->global_variables.size;
+  slice_push(&ctx->global_variables, variable);
+  return index;
+}
+
+usz cbe_find_or_add_symbol(struct cbe_context *ctx, cstr symbol) {
+  usz index = cbe_find_symbol(ctx, symbol);
+  if (index != SIZE_MAX)
+    return index;
+
+  return cbe_add_symbol(ctx, symbol);
+}
+
+usz cbe_find_symbol(struct cbe_context *ctx, cstr symbol) {
+  for (usz i = 0; i < ctx->symbol_table.size; i++) {
+    cstr s = ctx->symbol_table.items[i];
+    if (strncmp(s, symbol, strlen(symbol)) == 0)
+      return i;
+  }
+
+  return SIZE_MAX;
+}
+
+usz cbe_add_symbol(struct cbe_context *ctx, cstr symbol) {
+  slice_push(&ctx->symbol_table, symbol);
+  return ctx->symbol_table.size - 1;
 }
 
 const char *cbe_register_name(usz index) {
--- cbe.h
+++ cbe.h
@@ -41,6 +41,17 @@
   } while (0)
 
 typedef const char *cstr;
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+typedef long isz;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 typedef unsigned long usz;
 
 enum {
@@ -85,6 +96,8 @@ enum cbe_type_tag {
 
   CBE_TYPE_RAWPTR,
   CBE_TYPE_PTR,
+
+  CBE_TYPE_VOID,
 };
 
 typedef usz cbe_type_id;
@@ -95,7 +108,63 @@ struct cbe_type {
   };
 };
 
+enum cbe_value_tag {
+  CBE_VALUE_INTEGER,
+  CBE_VALUE_STRING,
+  CBE_VALUE_VARIABLE,
+};
+struct cbe_value {
+  enum cbe_value_tag tag;
+  cbe_type_id type_id;
+  union {
+    i64 integer;
+    cstr string;
+    usz variable;
+  };
+};
+
+enum cbe_instruction_tag {
+  CBE_INST_ALLOC, /* %0 = alloc <type> */
+  CBE_INST_STORE, /* store <typed value>, <typed value> */
+  CBE_INST_LOAD,  /* */
+  CBE_INST_RET,   /* */
+};
+struct cbe_instruction {
+  enum cbe_instruction_tag tag;
+  struct {
+    usz name_index;
+    cbe_type_id type_id;
+  } * temporary;
+  union {
+    struct {
+      cbe_type_id type;
+    } alloc;
+    struct {
+      struct cbe_value value, pointer;
+    } store;
+    struct {
+      struct cbe_value pointer;
+    } load;
+    struct {
+      struct cbe_value *value;
+    } ret;
+  };
+};
+
+struct cbe_block {
+  usz name_index;
+  slice(struct cbe_instruction) instructions;
+};
+
+struct cbe_function {
+  usz name_index;
+  cbe_type_id type_id;
+  slice(struct cbe_block) blocks;
+};
+
 struct cbe_global_variable {
+  usz name_index;
+  struct cbe_value value;
   bool constant; // Basically just specifies if the global is defined in the
                  // rodata or data section.
 };
@@ -104,6 +173,12 @@ struct cbe_context {
   struct cbe_register registers[CBE_REG_COUNT];
   slice(struct cbe_stack_variable) stack_variables;
   slice(struct cbe_global_variable) global_variables;
+  slice(cstr) symbol_table;
+};
+
+enum cbe_validation_result {
+  CBE_VALID_OK,
+  CBE_VALID_TYPE_MISMATCH,
 };
 
 void cbe_init(struct cbe_context *);
@@ -113,6 +188,22 @@ usz cbe_allocate_register(struct cbe_context *);
 void cbe_free_register(struct cbe_context *, usz);
 
 usz cbe_allocate_stack_variable(struct cbe_context *);
+
+usz cbe_new_global_variable(struct cbe_context *, struct cbe_global_variable);
+
+usz cbe_find_or_add_symbol(struct cbe_context *, cstr);
+usz cbe_find_symbol(struct cbe_context *, cstr);
+usz cbe_add_symbol(struct cbe_context *, cstr);
+
+enum cbe_validation_result validate_function(struct ctx_context *,
+                                             struct cbe_function);
+enum cbe_validation_result validate_block(struct ctx_context *,
+                                          struct cbe_block);
+enum cbe_validation_result validate_instruction(struct ctx_context *,
+                                                struct cbe_instruction);
+enum cbe_validation_result validate_value(struct ctx_context *,
+                                          struct cbe_value);
+enum cbe_validation_result validate_type(struct ctx_context *, struct cbe_type);
 
 const char *cbe_register_name(usz);
 
